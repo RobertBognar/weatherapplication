71615cbd540b23f391973345a9253bbcd42cb4a8
RobertBognar <robertbognar2@gmail.com>
2021-06-23 12:59:03 +0200

    simple weather API application

---
+++ scripts/app.js
@@ -0,0 +1,72 @@
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+const forecast = new Forecast();
+
+
+const updateUI = (data) => {
+
+    // const cityDets = data.cityDets;
+    // const weather = data.weather;
+    console.log(data);
+    //destructuring proprerties
+    const { cityDets, weather } = data;
+
+    //update details template, englishname and weathertext using from console to display what type of weather is and city name
+    details.innerHTML = `
+        <h5 class="my-3">${cityDets.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+            <span>${weather.Temperature.Metric.Value}</span>
+            <span>&deg;C</span>
+        </div>
+    `;
+
+    //update images for night or day, using let because we are going to over write it
+    //icorSrc changing to cloudy or sunny from svg folder
+
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+
+    //second solution is to use ternary operator
+
+    let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+    // if(weather.IsDayTime === true){
+    //     timeSrc = 'img/day.svg';
+    // } else {
+    //     timeSrc = 'img/night.svg';
+    // }
+    time.setAttribute('src', timeSrc);
+
+    //remove d-none class if present
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none');
+    };
+
+
+};
+
+cityForm.addEventListener('submit', e => {
+    //stop refreshing the page, prevent default action
+    e.preventDefault();
+
+    //get city value
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+
+    //update the ui with the new city value
+    forecast.updateCity(city)
+    .then(data => updateUI(data))
+    .catch(err => console.log('error'));
+
+    //setting up the local storage
+    localStorage.setItem('citi', city);
+});
+
+if(localStorage.getItem('city')){
+    forecast.updateCity(localStorage.getItem('city'))
+    .then(data => updateUI(data))
+    .catch(err => console.log(err));
+}
---
+++ scripts/forecast.js
@@ -0,0 +1,61 @@
+//USING CLASS
+
+class Forecast{
+    constructor(){
+        this.key = '4eN9NtNoFhfWMg1MQA2afH3qIqE0So0j';
+        this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
+        this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    }
+    async updateCity(city){
+        const cityDets = await this.getCity(city);
+        const weather = await this.getWeather(cityDets.Key);
+        return{ cityDets, weather };
+    }
+
+    async getCity(city){
+        
+        const query = `?apikey=${this.key}&q=${city}`;
+        const response = await fetch(this.cityURI + query);
+        const data = await response.json();
+        return data[0];
+    }
+
+    async getWeather(id){
+        const query = `${id}?apikey=${this.key}`;
+        const response = await fetch(this.weatherURI + query);
+        const data = await response.json();
+        return data[0];
+    }
+}
+
+//AccuWeather API KEY
+// const key = '4eN9NtNoFhfWMg1MQA2afH3qIqE0So0j';
+
+//get weather information function
+
+// const getWeather = async (id) => {
+
+//     const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+//     const query = `${id}?apikey=${key}`;
+
+//     const response = await fetch(base + query);
+//     const data = await response.json();
+
+//     return data[0];
+
+// };
+
+
+// //get the city information
+// const getCity = async (city) => {
+//     //API endpoint
+//     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+//     const query = `?apikey=${key}&q=${city}`;
+
+//     const response = await fetch(base + query);
+//     //transfering to json data
+//     const data = await response.json();
+
+//     return data[0];
+
+// };
